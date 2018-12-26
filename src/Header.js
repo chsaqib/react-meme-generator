afd19196a2b524b1995057e7345d4f98bc073867
Saqib Nazir <saqib_9090@yahoo.com>
2018-12-26 22:42:48 +0100

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,10 @@ class Header extends Component {
   render() {
     return (
       <header>
-        {/* <img
+         <img
           src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
           alt="Problem?"
-        /> */}
+        /> 
         <p>Meme Generator</p>
       </header>
     );
