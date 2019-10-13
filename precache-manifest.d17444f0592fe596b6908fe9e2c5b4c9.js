679e761aaf995e09825f849ec2e1eefc0d837aa6
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-13 18:17:51 +0200

    Updates

---
+++ precache-manifest.d17444f0592fe596b6908fe9e2c5b4c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b44c4de7178c9d0f378",
+    "url": "/react-meme-generator/static/js/runtime~main.3b44c4de.js"
+  },
+  {
+    "revision": "36b5b37911794bc61bd7",
+    "url": "/react-meme-generator/static/js/main.36b5b379.chunk.js"
+  },
+  {
+    "revision": "11dac3cdf202915bf55a",
+    "url": "/react-meme-generator/static/js/1.11dac3cd.chunk.js"
+  },
+  {
+    "revision": "36b5b37911794bc61bd7",
+    "url": "/react-meme-generator/static/css/main.485494e3.chunk.css"
+  },
+  {
+    "revision": "1cccb69aac81b6389677facce03e09b4",
+    "url": "/react-meme-generator/index.html"
+  }
+];
